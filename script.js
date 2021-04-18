1636cbc4f0f479a816f587d590219ac6b7ff6dd5
rasyidf <ufarasfa@gmail.com>
2021-04-18 22:19:28 +0700

    Update Ajax Todo

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
     $("#Ambil").click(function (e) {
-        e.preventDefault(); 
+        e.preventDefault();
         $.ajax({
             type: "GET",
             crossDomain: true, headers: { 'Access-Control-Allow-Origin': '*' },
-            url: "https://jsonplaceholder.typicode.com/todos/1",
+            url: "https://jsonplaceholder.typicode.com/todos",
             success: function (result) {
-                $("#hasil").html(result);
+                console.table(result);
+                let data = '';
+                $.each(result, function (i, res) {
+                    data += `<li class="list-group-item container-fluid"> 
+                    <div class="row">
+                       <div class="col-2">  <span class="badge `+ ((res.completed) ? 'bg-success' : 'bg-secondary') + `">` + ((res.completed) ? 'Selesai' : 'Belum Selesai') + `</span>
+                       </div>
+                       <div class="col">
+                       `+ res.title + `
+                       </div>
+                    </div>
+                  </li>\n` ;
+                })
+                $("#hasil").html(data);
             }
         }
         );
